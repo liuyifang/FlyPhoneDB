fb9775335f883153178606d989c171c7d9ccbb25
liuyifang <liuyifang3000@gmail.com>
2020-05-22 08:51:07 -0400

    add hist

--- app.R
+++ app.R
@@ -1,44 +1,56 @@
 # Load R packages
 library(shiny)
 library(shinythemes)
+data(airquality)
+
+# Define UI
+ui <- fluidPage(theme = shinytheme("yeti"),
+                navbarPage(
+                  # theme = "cerulean",  # <--- To use a theme, uncomment this
+                  "My first app",
+                  tabPanel("Ozone level!",
+                           # Sidebar panel for inputs ----
+                           sidebarPanel(
+
+                             # Input: Slider for the number of bins ----
+                             sliderInput(inputId = "bins",
+                                         label = "Number of bins:",
+                                         min = 0,
+                                         max = 50,
+                                         value = 30,
+                                         step = 2)
+
+                           ),
+                           mainPanel(
+                             # Output: Histogram ----
+                             plotOutput(outputId = "distPlot")
+
+                           ) # mainPanel
+
+                  ), # Navbar 1, tabPanel
+                  tabPanel("Navbar 2", "This panel is intentionally left blank"),
+                  tabPanel("Navbar 3", "This panel is intentionally left blank")
+
+                ) # navbarPage
+) # fluidPage
 
 
-  # Define UI
-  ui <- fluidPage(theme = shinytheme("yeti"),
-    navbarPage(
-      # theme = "cerulean",  # <--- To use a theme, uncomment this
-      "My first app",
-      tabPanel("Navbar 1",
-               sidebarPanel(
-                 tags$h3("Input:"),
-                 textInput("txt1", "Given Name:", "hello"), # txt1 will be send to server
-                 textInput("txt2", "Surname:", "mao"),    # txt2 will be send to server
+# Define server function
+server <- function(input, output) {
 
-               ), # sidebarPanel
-               mainPanel(
-                            h1("Header 1"),
+  output$distPlot <- renderPlot({
 
-                            h4("Output 1"),
-                            verbatimTextOutput("txtout"), # txtout
+    x    <- airquality$Ozone
+    x    <- na.omit(x)
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-               ) # mainPanel
+    hist(x, breaks = bins, col = "green", border = "black",
+         xlab = "Ozone level",
+         main = "Histogram of Ozone level")
 
-      ), # Navbar 1, tabPanel
-      tabPanel("Navbar 2", "This panel is intentionally left blank"),
-      tabPanel("Navbar 3", "This panel is intentionally left blank")
+  })
 
-    ) # navbarPage
-  ) # fluidPage
+} # server
 
-
-  # Define server function
-  server <- function(input, output) {
-
-    output$txtout <- renderText({
-      paste( input$txt1, input$txt2, sep = " " )
-    })
-  } # server
-
-
-  # Create Shiny object
-  shinyApp(ui = ui, server = server)
+# Create Shiny object
+shinyApp(ui = ui, server = server)
