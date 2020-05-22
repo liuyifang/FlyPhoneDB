c42b7f0a100e5f8ee39e89dd79b52b62fc6e3d08
liuyifang <liuyifang3000@gmail.com>
2020-05-22 15:21:25 -0400

    split some code to prepare_data.R

--- app.R
+++ app.R
@@ -2,21 +2,14 @@
 library(shiny)
 library(shinythemes)
 library(data.table)
-library(RCurl)
 library(randomForest)
-
-# # Read data
-# weather <- read.csv(text = getURL("https://raw.githubusercontent.com/dataprofessor/data/master/weather-weka.csv") )
-#
-# # Build model
-# model <- randomForest(play ~ ., data = weather, ntree = 500, mtry = 4, importance = TRUE)
-
-# Save model to RDS file
-# saveRDS(model, "model.rds")
 
 # Read in the RF model
 model <- readRDS("model.rds")
-# print(model)
+
+# Training set
+TrainSet <- read.csv("training.csv", header = TRUE)
+TrainSet <- TrainSet[,-1]
 
 ####################################
 # User interface                   #
@@ -27,28 +20,31 @@ ui <- fluidPage(theme = shinytheme("yeti"),
                 navbarPage(
                   # theme = "cerulean",  # <--- To use a theme, uncomment this
                   "CLP",
-                  tabPanel("Play Golf?",
+                  tabPanel("Iris Predictor",
+
                            # Input values
                            sidebarPanel(
-                             HTML("<h3>Input parameters</h3>"),
-
-                             selectInput("outlook", label = "Outlook:",
-                                         choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"),
-                                         selected = "Sunny"),
-                             sliderInput("temperature", label = "Temperature:",
-                                         min = 64, max = 86,
-                                         value = 70),
-                             sliderInput("humidity", label = "Humidity:",
-                                         min = 65, max = 96,
-                                         value = 90),
-                             selectInput("windy", label = "Windy:",
-                                         choices = list("Yes" = "TRUE", "No" = "FALSE"),
-                                         selected = "TRUE"),
+                             #HTML("<h3>Input parameters</h3>"),
+                             tags$label(h3("Input parameters")),
+                             sliderInput("Sepal.Length", label = "Sepal Length", value = 5.0,
+                                         min = min(TrainSet$Sepal.Length),
+                                         max = max(TrainSet$Sepal.Length)
+                             ),
+                             sliderInput("Sepal.Width", label = "Sepal Width", value = 3.6,
+                                         min = min(TrainSet$Sepal.Width),
+                                         max = max(TrainSet$Sepal.Width)),
+                             sliderInput("Petal.Length", label = "Petal Length", value = 1.4,
+                                         min = min(TrainSet$Petal.Length),
+                                         max = max(TrainSet$Petal.Length)),
+                             sliderInput("Petal.Width", label = "Petal Width", value = 0.2,
+                                         min = min(TrainSet$Petal.Width),
+                                         max = max(TrainSet$Petal.Width)),
 
                              actionButton("submitbutton", "Submit", class = "btn btn-primary")
                            ),
+
                            mainPanel(
-                             tags$label(h3("Status/Output")), # Status/Output Text Box
+                             tags$label(h3("Status/Output")), # Status/Output Text Box, same as HTML
                              verbatimTextOutput("contents"),
                              tableOutput("tabledata") # Prediction results table
                            ) # mainPanel
@@ -70,29 +66,23 @@ server <- function(input, output, session) {
   # Input Data
   datasetInput <- reactive({
 
-    # outlook,temperature,humidity,windy,play
     df <- data.frame(
-      Name = c("outlook",
-               "temperature",
-               "humidity",
-               "windy"),
-      Value = as.character(c(input$outlook,
-                             input$temperature,
-                             input$humidity,
-                             input$windy)),
+      Name = c("Sepal Length",
+               "Sepal Width",
+               "Petal Length",
+               "Petal Width"),
+      Value = as.character(c(input$Sepal.Length,
+                             input$Sepal.Width,
+                             input$Petal.Length,
+                             input$Petal.Width)),
       stringsAsFactors = FALSE)
 
-    play <- "play"
-    df <- rbind(df, play)
+    Species <- 0
+    df <- rbind(df, Species)
     input <- transpose(df)
-
-    # export to file, this is a good way to debug
     write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
     test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
-
-    test$outlook <- factor(test$outlook, levels = c("overcast", "rainy", "sunny"))
-
 
     Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
     print(Output)
@@ -123,3 +113,10 @@ server <- function(input, output, session) {
 
 # Create Shiny object
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
